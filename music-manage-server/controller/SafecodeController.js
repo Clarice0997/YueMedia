c63b55e9c511dd07f89361021311326d8a38cc11
Clarice0997 <clarice09975@gmail.com>
2023-03-27 13:23:04 +0800

    pref: try catch处理验证码生成抛出异常

--- music-manage-server/controller/SafecodeController.js
+++ music-manage-server/controller/SafecodeController.js
@@ -12,16 +12,17 @@ router.get('/', async (req, res) => {
   const { type } = req.query
 
   // Service
-  const { code, data } = await generateSafeCode(type)
-
-  // response
-  if (code === 200) {
-    // response header
-    res.setHeader('Content-Type', 'image/svg+xml')
-    // response cookie
-    res.cookie('uuid', data.uuid)
-    res.status(code).send(String(data.svg))
-  } else {
+  try {
+    const { code, data } = await generateSafeCode(type)
+    // response
+    if (code === 200) {
+      // response header
+      res.setHeader('Content-Type', 'image/svg+xml')
+      // response cookie
+      res.cookie('uuid', data.uuid)
+      res.status(code).send(String(data.svg))
+    }
+  } catch ({ code, data }) {
     res.status(code).send(data)
   }
 })
